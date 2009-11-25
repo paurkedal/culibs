8074c1c3da58cbc7fd4e073ccef82f97e9e02e38
Petter Urkedal <urkedal@nbi.dk>
2009-11-25 20:04:45 +0100

    Add documentation and tidy cuflow/workers.*.
    cuflow/workers.h:
      * Document the remaining functions.
      * Change doc comment style.
    cuflow/workers.c:
      * Prefix static identifiers with underscore.

--- cuflow/workers.c
+++ cuflow/workers.c
@@ -27,107 +27,108 @@
 
 cu_dlog_def(_file, "dtag=cuflow.workers");
 
-typedef struct cuflow_worker_s *cuflow_worker_t;
-typedef struct cuflow_atjob_s *cuflow_atjob_t;
+typedef struct _atjob  *_atjob_t;
+typedef struct _worker *_worker_t;
 
-struct cuflow_atjob_s
+struct _atjob
 {
     struct timespec t_call;
     cu_clop(callback, void, struct timespec *current_time);
 };
-struct cuflow_worker_s
+
+struct _worker
 {
     pthread_t thread;
     cu_bool_t do_shutdown;
 };
 
-static pthread_mutex_t		work_mutex = CU_MUTEX_INITIALISER;
-static pthread_cond_t		work_cond = PTHREAD_COND_INITIALIZER;
-static struct cucon_priq	work_atq;
-static struct cucon_list	work_nowlist;
-static struct cucon_list	work_scheduler_list;
+static pthread_mutex_t		_work_mutex = CU_MUTEX_INITIALISER;
+static pthread_cond_t		_work_cond = PTHREAD_COND_INITIALIZER;
+static struct cucon_priq	_work_atq;
+static struct cucon_list	_work_nowlist;
+static struct cucon_list	_work_scheduler_list;
 
-static pthread_mutex_t		workers_mutex = CU_MUTEX_INITIALISER;
-static struct cucon_list	workers_list; /* of struct cuflow_worker_s */
-static AO_t			workers_count = 0;
+static pthread_mutex_t		_workers_mutex = CU_MUTEX_INITIALISER;
+static struct cucon_list	_workers_list; /* of struct _worker */
+static AO_t			_workers_count = 0;
 
 /* Other parts doing "on signal" work can increment this when there is work to
- * be done, so to prevent threads from sleeping on work_cond.  It can then
+ * be done, so to prevent threads from sleeping on _work_cond.  It can then
  * broadcast to wake up those threads which are already sleeping on
- * work_cond. */
+ * _work_cond. */
 AO_t cuflowP_pending_work = 0;
 
 AO_t cuflowP_workers_waiting_count = 0;
 
 
-cu_clop_def(atjob_before_clop, cu_bool_t, void *atjob0, void *atjob1)
+cu_clop_def(_atjob_before_clop, cu_bool_t, void *atjob0, void *atjob1)
 {
-    return cuflow_timespec_lt(&((cuflow_atjob_t)atjob0)->t_call,
-			      &((cuflow_atjob_t)atjob1)->t_call);
+    return cuflow_timespec_lt(&((_atjob_t)atjob0)->t_call,
+			      &((_atjob_t)atjob1)->t_call);
 }
 
 void
 cuflow_workers_register_scheduler(cu_clop(sched, void, cu_bool_t))
 {
-    cu_mutex_lock(&work_mutex);
-    cucon_list_append_ptr(&work_scheduler_list, (void *)sched);
-    cu_mutex_unlock(&work_mutex);
+    cu_mutex_lock(&_work_mutex);
+    cucon_list_append_ptr(&_work_scheduler_list, (void *)sched);
+    cu_mutex_unlock(&_work_mutex);
 }
 
 static void
-worker_atrun(cuflow_worker_t worker)
+_worker_atrun(_worker_t worker)
 {
     struct timespec now;
     clock_gettime(CLOCK_REALTIME, &now);
-    while (!cucon_priq_is_empty(&work_atq)) {
-	cuflow_atjob_t atjob = cucon_priq_front(&work_atq);
-	cuflow_atjob_t atjob_next;
+    while (!cucon_priq_is_empty(&_work_atq)) {
+	_atjob_t atjob = cucon_priq_front(&_work_atq);
+	_atjob_t atjob_next;
 	if (cuflow_timespec_lt(&now, &atjob->t_call))
 	    break;
-	cucon_priq_pop_front(&work_atq);
+	cucon_priq_pop_front(&_work_atq);
 
 	/* Signal more worker threads in case there is more to do right
 	 * away. */
-	atjob_next = cucon_priq_front(&work_atq);
+	atjob_next = cucon_priq_front(&_work_atq);
 	if (cuflow_timespec_lt(&now, &atjob_next->t_call))
-	    pthread_cond_signal(&work_cond);
+	    pthread_cond_signal(&_work_cond);
 
-	cu_mutex_unlock(&work_mutex);
+	cu_mutex_unlock(&_work_mutex);
 	cu_call(atjob->callback, &now);
-	cu_mutex_lock(&work_mutex);
+	cu_mutex_lock(&_work_mutex);
     }
 }
 
 void
 cuflowP_workers_broadcast(void)
 {
-    cu_mutex_lock(&work_mutex);
-    pthread_cond_broadcast(&work_cond);
-    cu_mutex_unlock(&work_mutex);
+    cu_mutex_lock(&_work_mutex);
+    pthread_cond_broadcast(&_work_cond);
+    cu_mutex_unlock(&_work_mutex);
 }
 
 static void
 cuflowP_yield_lck(cu_bool_t is_global)
 {
     cucon_listnode_t itr;
-    for (itr = cucon_list_begin(&work_scheduler_list);
-	 itr != cucon_list_end(&work_scheduler_list);
+    for (itr = cucon_list_begin(&_work_scheduler_list);
+	 itr != cucon_list_end(&_work_scheduler_list);
 	 itr = cucon_listnode_next(itr)) {
 	cu_clop(callback, void, cu_bool_t);
 	cu_debug_assert(itr);
 	callback = cucon_listnode_ptr(itr);
-	cu_mutex_unlock(&work_mutex);
+	cu_mutex_unlock(&_work_mutex);
 	cu_call(callback, is_global);
-	cu_mutex_lock(&work_mutex);
+	cu_mutex_lock(&_work_mutex);
     }
 }
 
 void
 cuflowP_yield(void)
 {
-    cu_mutex_lock(&work_mutex);
+    cu_mutex_lock(&_work_mutex);
     cuflowP_yield_lck(cu_false);
-    cu_mutex_unlock(&work_mutex);
+    cu_mutex_unlock(&_work_mutex);
 }
 
 #if 0
@@ -145,40 +146,40 @@ cuflowP_cdisj_wait_while(AO_t *cdisj, cu_bool_t cond_val)
 
 	/* but then it's possible that other threads are working on the last
 	 * prerequisites, so prepare to wait. */
-	cu_mutex_lock(&work_mutex);
+	cu_mutex_lock(&_work_mutex);
 
 	/* Notify that we are going to wait.  This forces other threads to
-	 * sync on work_mutex and signal us if they enqueue any work. */
+	 * sync on _work_mutex and signal us if they enqueue any work. */
 	AO_fetch_and_add1_acquire(&cuflowP_workers_waiting_count);
 
 	/* Then a last check if condition is fulfilled, */
 	if (!!AO_load_acquire(cdisj) == cond_val) {
 	    AO_fetch_and_sub1(&cuflowP_workers_waiting_count);
-	    cu_mutex_unlock(&work_mutex);
+	    cu_mutex_unlock(&_work_mutex);
 	    return;
 	}
 	/* or if more work is present before we sleep */
 	if (!AO_load_acquire(&cuflowP_pending_work))
-	    pthread_cond_wait(&work_cond, &work_mutex);
+	    pthread_cond_wait(&_work_cond, &_work_mutex);
 	AO_fetch_and_sub1(&cuflowP_workers_waiting_count);
-	cu_mutex_unlock(&work_mutex);
+	cu_mutex_unlock(&_work_mutex);
     } while (!AO_load_acquire(cdisj) == cond_val);
 }
 #endif
 
 static void *
-worker_main(void *worker)
+_worker_main(void *worker)
 {
-#define worker ((cuflow_worker_t)worker)
-    cu_mutex_lock(&work_mutex);
+#define worker ((_worker_t)worker)
+    cu_mutex_lock(&_work_mutex);
     while (!worker->do_shutdown) {
 	cu_clop0(callback, void);
-	while (!cucon_list_is_empty(&work_nowlist)) {
-	    callback = cucon_list_front_ptr(&work_nowlist);
-	    cucon_list_pop_front(&work_nowlist);
-	    cu_mutex_unlock(&work_mutex);
+	while (!cucon_list_is_empty(&_work_nowlist)) {
+	    callback = cucon_list_front_ptr(&_work_nowlist);
+	    cucon_list_pop_front(&_work_nowlist);
+	    cu_mutex_unlock(&_work_mutex);
 	    cu_call0(callback);
-	    cu_mutex_lock(&work_mutex);
+	    cu_mutex_lock(&_work_mutex);
 	    if (worker->do_shutdown)
 		goto break_0;
 	}
@@ -188,14 +189,14 @@ worker_main(void *worker)
 	    break;
 	AO_fetch_and_add1(&cuflowP_workers_waiting_count);
 	if (!AO_load_acquire(&cuflowP_pending_work)) {
-	    if (cucon_priq_is_empty(&work_atq))
-		pthread_cond_wait(&work_cond, &work_mutex);
+	    if (cucon_priq_is_empty(&_work_atq))
+		pthread_cond_wait(&_work_cond, &_work_mutex);
 	    else {
-		cuflow_atjob_t atjob = cucon_priq_front(&work_atq);
+		_atjob_t atjob = cucon_priq_front(&_work_atq);
 		int err;
 		cu_debug_assert(atjob->t_call.tv_nsec >= 0);
 		cu_debug_assert(atjob->t_call.tv_nsec < 1000000000);
-		err = pthread_cond_timedwait(&work_cond, &work_mutex,
+		err = pthread_cond_timedwait(&_work_cond, &_work_mutex,
 					     &atjob->t_call);
 		cu_debug_assert(atjob->t_call.tv_nsec >= 0);
 		cu_debug_assert(atjob->t_call.tv_nsec < 1000000000);
@@ -205,7 +206,7 @@ worker_main(void *worker)
 		    case ETIMEDOUT:
 			if (worker->do_shutdown)
 			    break;
-			worker_atrun(worker);
+			_worker_atrun(worker);
 			break;
 		    default:
 			cu_bugf("Unexpected failure of "
@@ -216,8 +217,8 @@ worker_main(void *worker)
 	AO_fetch_and_sub1(&cuflowP_workers_waiting_count);
     }
 break_0:
-    pthread_cond_signal(&work_cond);
-    cu_mutex_unlock(&work_mutex);
+    pthread_cond_signal(&_work_cond);
+    cu_mutex_unlock(&_work_mutex);
     return NULL;
 #undef worker
 }
@@ -225,134 +226,134 @@ break_0:
 int
 cuflow_workers_count()
 {
-    return AO_load_acquire_read(&workers_count);
+    return AO_load_acquire_read(&_workers_count);
 }
 
 static void
-workers_drop_lck(int target_count)
+_workers_drop_lck(int target_count)
 {
     do {
 	int last_workers_count;
-	cuflow_worker_t worker;
+	_worker_t worker;
 
-	cu_debug_assert(!cucon_list_is_empty(&workers_list));
-	worker = cucon_list_front_mem(&workers_list);
-	cucon_list_pop_front(&workers_list);
-	--workers_count;
-	last_workers_count = workers_count;
+	cu_debug_assert(!cucon_list_is_empty(&_workers_list));
+	worker = cucon_list_front_mem(&_workers_list);
+	cucon_list_pop_front(&_workers_list);
+	--_workers_count;
+	last_workers_count = _workers_count;
 
-	cu_mutex_unlock(&workers_mutex);
-	cu_mutex_lock(&work_mutex);
+	cu_mutex_unlock(&_workers_mutex);
+	cu_mutex_lock(&_work_mutex);
 	worker->do_shutdown = cu_true;
-	cu_mutex_unlock(&work_mutex);
+	cu_mutex_unlock(&_work_mutex);
 	cu_dlogf(_file,
-		 "Shutting down %p, workers_count=%d, workers_waiting=%d",
-		 worker, workers_count, cuflowP_workers_waiting_count);
+		 "Shutting down %p, _workers_count=%d, workers_waiting=%d",
+		 worker, _workers_count, cuflowP_workers_waiting_count);
 	cuflow_workers_broadcast();
 	pthread_join(worker->thread, NULL);
-	cu_mutex_lock(&workers_mutex);
+	cu_mutex_lock(&_workers_mutex);
 
-	/* If strict monononicity of workers_count is broken, it means
-	 * that another thread increased the number of workers while
-	 * workers_mutex was unlocked.  If so, give up. */
-	if (workers_count > last_workers_count)
+	/* If strict monononicity of _workers_count is broken, it means that
+	 * another thread increased the number of workers while _workers_mutex
+	 * was unlocked.  If so, give up. */
+	if (_workers_count > last_workers_count)
 	    break; /* Another thread is trying to spawn new workers. */
-    } while (target_count < workers_count);
+    } while (target_count < _workers_count);
 }
 
 static void
-workers_spawn_lck(int target_count)
+_workers_spawn_lck(int target_count)
 {
-    while (target_count > workers_count) {
+    while (target_count > _workers_count) {
 	cucon_listnode_t worker_it;
-	cuflow_worker_t worker;
+	_worker_t worker;
 
-	worker_it = cucon_list_append_mem(&workers_list,
-					  sizeof(struct cuflow_worker_s));
+	worker_it = cucon_list_append_mem(&_workers_list,
+					  sizeof(struct _worker));
 	worker = cucon_listnode_mem(worker_it);
 	worker->do_shutdown = cu_false;
-	cu_pthread_create(&worker->thread, NULL, worker_main, worker);
-	++workers_count;
+	cu_pthread_create(&worker->thread, NULL, _worker_main, worker);
+	++_workers_count;
     }
 }
 
 void
 cuflow_workers_spawn(int target_count)
 {
-    cu_mutex_lock(&workers_mutex);
-    if (target_count < workers_count)
-	workers_drop_lck(target_count);
-    else if (target_count > workers_count)
-	workers_spawn_lck(target_count);
-    cu_mutex_unlock(&workers_mutex);
+    cu_mutex_lock(&_workers_mutex);
+    if (target_count < _workers_count)
+	_workers_drop_lck(target_count);
+    else if (target_count > _workers_count)
+	_workers_spawn_lck(target_count);
+    cu_mutex_unlock(&_workers_mutex);
 }
 
 void
 cuflow_workers_spawn_at_most(int target_count)
 {
-    cu_mutex_lock(&workers_mutex);
-    if (target_count < workers_count)
-	workers_drop_lck(target_count);
-    cu_mutex_unlock(&workers_mutex);
+    cu_mutex_lock(&_workers_mutex);
+    if (target_count < _workers_count)
+	_workers_drop_lck(target_count);
+    cu_mutex_unlock(&_workers_mutex);
 }
 
 void
 cuflow_workers_spawn_at_least(int target_count)
 {
-    cu_mutex_lock(&workers_mutex);
-    if (target_count > workers_count)
-	workers_spawn_lck(target_count);
-    cu_mutex_unlock(&workers_mutex);
+    cu_mutex_lock(&_workers_mutex);
+    if (target_count > _workers_count)
+	_workers_spawn_lck(target_count);
+    cu_mutex_unlock(&_workers_mutex);
 }
 
 void
 cuflow_workers_call_at(cu_clop(callback, void, struct timespec *t_now),
 		       struct timespec *t_call)
 {
-    cuflow_atjob_t atjob = cu_gnew(struct cuflow_atjob_s);
-    cuflow_atjob_t atjob_prev_head;
+    _atjob_t atjob = cu_gnew(struct _atjob);
+    _atjob_t atjob_prev_head;
 
     cu_debug_assert(atjob->t_call.tv_nsec < 1000000000);
     cu_debug_assert(atjob->t_call.tv_nsec >= 0);
     memcpy(&atjob->t_call, t_call, sizeof(struct timespec));
     atjob->callback = callback;
 
-    cu_mutex_lock(&work_mutex);
-    if (cucon_priq_is_empty(&work_atq))
+    cu_mutex_lock(&_work_mutex);
+    if (cucon_priq_is_empty(&_work_atq))
 	atjob_prev_head = NULL;
     else
-	atjob_prev_head = cucon_priq_front(&work_atq);
-    cucon_priq_insert(&work_atq, atjob);
+	atjob_prev_head = cucon_priq_front(&_work_atq);
+    cucon_priq_insert(&_work_atq, atjob);
 
-    /* Make sure we have at least one worker which will do the timed job.
-     * This worker will signal (from worker_atrun) another worker if there is
-     * more work to do right away. */
+    /* Make sure we have at least one worker which will do the timed job.  This
+     * worker will signal (from _worker_atrun) another worker if there is more
+     * work to do right away. */
     if (!atjob_prev_head || cuflow_timespec_lt(t_call, &atjob_prev_head->t_call))
-	pthread_cond_signal(&work_cond);
+	pthread_cond_signal(&_work_cond);
 
-    cu_mutex_unlock(&work_mutex);
+    cu_mutex_unlock(&_work_mutex);
 }
 
 void
 cuflow_workers_call(cu_clop0(callback, void))
 {
-    cu_mutex_lock(&work_mutex);
-    cucon_list_append_ptr(&work_nowlist, (void *)callback);
-    pthread_cond_signal(&work_cond);
-    cu_mutex_unlock(&work_mutex);
+    cu_mutex_lock(&_work_mutex);
+    cucon_list_append_ptr(&_work_nowlist, (void *)callback);
+    pthread_cond_signal(&_work_cond);
+    cu_mutex_unlock(&_work_mutex);
 }
 
 void
 cuflow_workers_call_on_signal(cu_clop0(callback, void))
 {
-    cu_mutex_lock(&work_mutex);
-    cucon_list_append_ptr(&work_scheduler_list, (void *)callback);
-    pthread_cond_signal(&work_cond);
-    cu_mutex_unlock(&work_mutex);
+    cu_mutex_lock(&_work_mutex);
+    cucon_list_append_ptr(&_work_scheduler_list, (void *)callback);
+    pthread_cond_signal(&_work_cond);
+    cu_mutex_unlock(&_work_mutex);
 }
 
 static void
-workers_cleanup(void)
+_workers_cleanup(void)
 {
     cuflow_workers_spawn(0);
 }
@@ -360,9 +361,9 @@ workers_cleanup(void)
 void
 cuflowP_workers_init()
 {
-    cucon_priq_init(&work_atq, atjob_before_clop);
-    cucon_list_init(&work_nowlist);
-    cucon_list_init(&work_scheduler_list);
-    cucon_list_init(&workers_list);
-    atexit(workers_cleanup);
+    cucon_priq_init(&_work_atq, _atjob_before_clop);
+    cucon_list_init(&_work_nowlist);
+    cucon_list_init(&_work_scheduler_list);
+    cucon_list_init(&_workers_list);
+    atexit(_workers_cleanup);
 }
--- cuflow/workers.h
+++ cuflow/workers.h
@@ -1,5 +1,5 @@
 /* Part of the culibs project, <http://www.eideticdew.org/culibs/>.
- * Copyright (C) 2006--2007  Petter Urkedal <urkedal@nbi.dk>
+ * Copyright (C) 2006--2009  Petter Urkedal <urkedal@nbi.dk>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,53 +25,63 @@
 #include <atomic_ops.h>
 
 CU_BEGIN_DECLARATIONS
-/*!\defgroup cuflow_workers_h cuflow/workers.h: Auxiliary Worker Threads
- *@{\ingroup cuflow_mod
- *
- * This header provides an API to launch one or more worker threads and run
- * functions within them, either as soon as one is idle or at a given time.
- * The main purpose of this is to unify such work in a common framework, so
- * that the same thread(s) can be used, rather than adding now threads for
- * each task.  The functions defined here is aimed to be the bare minimum to
- * achieve this.  For instance, if you need to know when a callback is
- * finised, you may need to supply it's closure struct with an additional
- * mutex and possibly a condition variable to communicate with consumer(s) of
- * the computation.
- *
- * This is implemenated purely with POSIX threads, mutices and condition
- * variables, and in particular we avoid timers or signals which could have
- * caused trouble in a multi-threaded environment.
- */
-
-/*!The current number of worker threads. */
-int cuflow_workers_count(void);
-
-/*!Spawn or drops worker threads until there are exactly \a target_count left.
- * This is not safe to call within worker threads. */
-void cuflow_workers_spawn(int target_count);
-
-/*!Spawns worker threads as needed so that there are at least \a target_count
- * of them. */
-void cuflow_workers_spawn_at_least(int target_count);
-
-/*!Drops worker threads as needed so that there are at most \a target_count
- * left. */
-void cuflow_workers_spawn_at_most(int target_count);
-
-/*!Call \a callback in one of the worker threads, as soon as one is ready. */
-void cuflow_workers_call(cu_clop0(callback, void));
-
-/*!Call \a callback at absolute \c CLOCK_REALTIME \a t_call. */
-void cuflow_workers_call_at(cu_clop(callback, void, struct timespec *t_now), 
-			    struct timespec *t_call);
 
 extern AO_t cuflowP_pending_work;
 extern AO_t cuflowP_workers_waiting_count;
 void cuflowP_workers_broadcast(void);
 void cuflowP_yield(void);
 
-void cuflow_workers_register_scheduler(cu_clop(callback, void, cu_bool_t));
+/** \defgroup cuflow_workers_h cuflow/workers.h: Auxiliary Worker Threads
+ ** @{\ingroup cuflow_mod
+ **
+ ** This header provides an API to launch one or more worker threads and run
+ ** functions within them, either as soon as one is idle or at a given time.
+ ** The main purpose of this is to unify such work in a common framework, so
+ ** that the same thread(s) can be used, rather than adding new threads for
+ ** each task.  The functions defined here is aimed to be the bare minimum to
+ ** achieve this.  For instance, if you need to know when a callback is
+ ** finised, you may need to supply it's closure struct with an additional
+ ** mutex and possibly a condition variable to communicate with consumer(s) of
+ ** the computation.
+ **
+ ** This is implemenated purely with POSIX threads, mutices and condition
+ ** variables, and in particular avoid timers or signals which may cause
+ ** trouble in a multi-threaded environment.
+ **/
 
+/** The current number of worker threads. */
+int cuflow_workers_count(void);
+
+/** Spawn or drop worker threads until there are exactly \a target_count left.
+ ** This is not safe to call within worker threads. */
+void cuflow_workers_spawn(int target_count);
+
+/** Spawns worker threads as needed so that there are at least \a target_count
+ ** of them. */
+void cuflow_workers_spawn_at_least(int target_count);
+
+/** Drops worker threads as needed so that there are at most \a target_count
+ ** left. */
+void cuflow_workers_spawn_at_most(int target_count);
+
+/** Call \a f in one of the worker threads, as soon as one is ready. */
+void cuflow_workers_call(cu_clop0(f, void));
+
+/** Call \a f at absolute \c CLOCK_REALTIME \a t_call. */
+void cuflow_workers_call_at(cu_clop(f, void, struct timespec *t_now),
+			    struct timespec *t_call);
+
+/** Arranged for \a sched to be called round-robin along with other registered
+ ** work.  This can happen either when a task is waiting for work to be
+ ** completed or directly from a worker thread.  When called from a worker
+ ** thread, true is passed for the first argument.  Otherwise, false is passed
+ ** to indicate that \a sched is called from an arbitrary subroutine, in which
+ ** case \a sched should only do a small amount of work and exit. */
+void cuflow_workers_register_scheduler(cu_clop(sched, void, cu_bool_t));
+
+/** A call to this function performs some work which have been registered by
+ ** one of the other functions from this header.  This is typcially invoked
+ ** when waiting for resources or results from other scheduled work. */
 CU_SINLINE void
 cuflow_yield(void)
 {
@@ -79,6 +89,9 @@ cuflow_yield(void)
 	cuflowP_yield();
 }
 
+/** Wake up any sleeping workers so that they can pick up any new work.  You
+ ** usually don't need to call this, since it's automically called by functions
+ ** which add new work or unlocks existing work. */
 CU_SINLINE void
 cuflow_workers_broadcast(void)
 {
@@ -86,6 +99,10 @@ cuflow_workers_broadcast(void)
 	cuflowP_workers_broadcast();
 }
 
+/** Indicate that your scheduler have pending work.  Pair it with a call to
+ ** \ref cuflow_workers_decr_pending when the work is done.  All schedulers
+ ** will run round-robin as long as this function has been called more times
+ ** than \ref cuflow_workers_decr_pending. */
 CU_SINLINE void
 cuflow_workers_incr_pending(void)
 {
@@ -93,13 +110,16 @@ cuflow_workers_incr_pending(void)
     cuflow_workers_broadcast();
 }
 
+/** Indicate that a unit of work signalled with \ref
+ ** cuflow_workers_incr_pending has been picked up.  This is typically called
+ ** from a custom scheduler. */
 CU_SINLINE void
 cuflow_workers_decr_pending(void)
 {
     AO_fetch_and_sub1(&cuflowP_pending_work);
 }
 
-/*!@}*/
+/** @} */
 CU_END_DECLARATIONS
 
 #endif
